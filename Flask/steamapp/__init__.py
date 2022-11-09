d94c13130356342e4702f5318b4694714b55b04b
BryanA999 <36612137+BryanA999@users.noreply.github.com>
2022-11-09 14:20:28 -0600

    Fleshing out the foundation
    * Reorganized directory - .html files now live in the templates folder so that they can be accessed from within Flask. README moved into app folder.
    
    * Added incredibly basic output page with dummy values
    
    * Added a secondary output page for when the review is empty

--- Flask/steamapp/__init__.py
+++ Flask/steamapp/__init__.py
@@ -1,21 +1,32 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, render_template, session
 app = Flask(__name__)
+app.secret_key = 'dev'
  
- 
-@app.route('/success/<text>')
-def success(text):
-    return 'Your Review: %s' % text
- 
- 
+@app.route('/success/')
+def success():
+    if(session.get('user_text', None)):
+        return render_template('output.html')
+    else:
+        return render_template('emptyReview.html')
+
+
 @app.route('/home', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
         user_text = request.form['user_review']
-        return redirect(url_for('success', text=user_text))
+        session['user_text'] = user_text
+        return redirect(url_for('success'))
     else:
         user = request.args.get('user_review')
-        return redirect(url_for('success', text=user_text))
- 
- 
+        return redirect(url_for('success'))
+
+@app.context_processor
+def getScores():
+    return {'score1':0.5,'score2':-0.6,'score3':-0.2,'score4':0.9}
+
+@app.route('/')
+def main():
+    return render_template('home.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
