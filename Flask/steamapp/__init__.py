f7a8de6e24cc83677a416c3ff95197d6798238e9
BryanA999 <36612137+BryanA999@users.noreply.github.com>
2022-11-11 13:39:00 -0600

    More CSS
    Moved outputStyle.css to the static folder so Flask can see it
    
    Added some nifty logic to generate an RGB color based on score
    
    Made the output a table so I could center the dang text

--- Flask/steamapp/__init__.py
+++ Flask/steamapp/__init__.py
@@ -1,7 +1,35 @@
 from flask import Flask, redirect, url_for, request, render_template, session
 app = Flask(__name__)
 app.secret_key = 'dev'
- 
+
+def scoreColor(score):
+    # error handling
+    if score < -1 or score > 1:
+        return 'DeepPink'
+    else:
+        # some really funky math to get a good-looking gradient
+        
+        # if score is within neutral range, map from rgb(100,100,100) at 0.0 to rgb(200,200,200) at +/-0.1
+        if score >= -0.1 and score <= 0.1:
+            B = 100 + abs(score)*1000
+        # otherwise, B=0 at +/-1, and linearly ramps to 135 at +/-0.1
+        else:
+            B = (1-abs(score))*150
+        # if within neutral range, make the color grey
+        if score <= 0.1:
+            G = B
+        # if score above the neutral threshold, max out the green value
+        else:
+            G = 225
+        # if within neutral range, make the color grey
+        if score >= -0.1:
+            R = B
+        # if score below the neutral threshold, max out the red value
+        else:
+            R = 225
+        
+        return f'rgb({R},{G},{B})'
+
 @app.route('/success/')
 def success():
     if(session.get('user_text', None)):
@@ -22,7 +50,11 @@ def home():
 
 @app.context_processor
 def getScores():
-    return {'score1':0.5,'score2':-0.6,'score3':-0.2,'score4':0.9}
+    score1 = -0.86
+    score2 = -0.31
+    score3 = 0.07
+    score4 = 0.75
+    return {'score1':score1,'score1_color':scoreColor(score1),'score2':score2,'score2_color':scoreColor(score2),'score3':score3,'score3_color':scoreColor(score3),'score4':score4,'score4_color':scoreColor(score4)}
 
 @app.route('/')
 def main():
