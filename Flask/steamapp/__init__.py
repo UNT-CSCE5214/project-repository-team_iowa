0f45a1e1f39a7225617b05531aa53098b91fe366
BryanA999 <36612137+BryanA999@users.noreply.github.com>
2022-11-11 15:12:08 -0600

    Refactoring
    Moved some stuff around: getScores is now a regular python function, and scoreColor is now in the context processor so it can be called from within HTML documents.
    
    Also, added a section for the user review on the output page

--- Flask/steamapp/__init__.py
+++ Flask/steamapp/__init__.py
@@ -2,33 +2,11 @@ from flask import Flask, redirect, url_for, request, render_template, session
 app = Flask(__name__)
 app.secret_key = 'dev'
 
-def scoreColor(score):
-    # error handling
-    if score < -1 or score > 1:
-        return 'DeepPink'
-    else:
-        # some really funky math to get a good-looking gradient
-        
-        # if score is within neutral range, map from rgb(100,100,100) at 0.0 to rgb(200,200,200) at +/-0.1
-        if score >= -0.1 and score <= 0.1:
-            B = 100 + abs(score)*1000
-        # otherwise, B=0 at +/-1, and linearly ramps to 135 at +/-0.1
-        else:
-            B = (1-abs(score))*150
-        # if within neutral range, make the color grey
-        if score <= 0.1:
-            G = B
-        # if score above the neutral threshold, max out the green value
-        else:
-            G = 225
-        # if within neutral range, make the color grey
-        if score >= -0.1:
-            R = B
-        # if score below the neutral threshold, max out the red value
-        else:
-            R = 225
-        
-        return f'rgb({R},{G},{B})'
+def getScores():
+        session['score1'] = -0.85
+        session['score2'] = -0.31
+        session['score3'] = 0.07
+        session['score4'] = 0.75
 
 @app.route('/success/')
 def success():
@@ -43,19 +21,43 @@ def home():
     if request.method == 'POST':
         user_text = request.form['user_review']
         session['user_text'] = user_text
+        getScores()
         return redirect(url_for('success'))
     else:
         user = request.args.get('user_review')
         return redirect(url_for('success'))
 
 @app.context_processor
-def getScores():
-    score1 = -0.86
-    score2 = -0.31
-    score3 = 0.07
-    score4 = 0.75
-    return {'score1':score1,'score1_color':scoreColor(score1),'score2':score2,'score2_color':scoreColor(score2),'score3':score3,'score3_color':scoreColor(score3),'score4':score4,'score4_color':scoreColor(score4)}
-
+def score_processor():
+    def scoreColor(score):
+        # error handling
+        if score < -1 or score > 1:
+            return 'DeepPink'
+        else:
+            # some really funky math to get a good-looking gradient
+            
+            # if score is within neutral range, map from rgb(100,100,100) at 0.0 to rgb(200,200,200) at +/-0.1
+            if score >= -0.1 and score <= 0.1:
+                B = 100 + abs(score)*1000
+            # otherwise, B=0 at +/-1, and linearly ramps to 135 at +/-0.1
+            else:
+                B = (1-abs(score))*150
+            # if within neutral range, make the color grey
+            if score <= 0.1:
+                G = B
+            # if score above the neutral threshold, max out the green value
+            else:
+                G = 225
+            # if within neutral range, make the color grey
+            if score >= -0.1:
+                R = B
+            # if score below the neutral threshold, max out the red value
+            else:
+                R = 225
+            
+            return f'rgb({R},{G},{B})'
+        
+    return dict(scoreColor=scoreColor)
 @app.route('/')
 def main():
     return render_template('home.html')
