a4050dc65994afa2ffffd53f31e947218b274b73
BryanA999 <36612137+BryanA999@users.noreply.github.com>
2022-11-14 14:58:42 -0600

    Haiyi's Model
    More CSS, added Haiyi's model to this app

--- Flask/steamapp/__init__.py
+++ Flask/steamapp/__init__.py
@@ -1,31 +1,51 @@
 from flask import Flask, redirect, url_for, request, render_template, session
+import pickle
+import re
+
 app = Flask(__name__)
 app.secret_key = 'dev'
+model1 = pickle.load(open('sentiment_analysis_best_model.pkl','rb'))
 
-def getScores():
-        session['score1'] = -0.85
-        session['score2'] = -0.31
+def getScores(text):
+        # model outputs 0 or 1. Multiply by 2 and subtract 1 to normalize to range [-1, 1]
+        session['score1'] = (float(model1.predict(process_text1(text))[0])*2.0)-1.0
+        session['score2'] = -0.11
         session['score3'] = 0.07
         session['score4'] = 0.75
 
+def process_text1(content):
+    sentence = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','', content)
+    sentence = re.sub('@[^\s]+','', sentence)
+    sentence = sentence.lower().split()
+    reformed_sentence = [word for word in sentence]
+    reformed_sentence = " ".join(reformed_sentence) 
+    sentence = re.sub('&[^\s]+;', '', reformed_sentence)
+    sentence = re.sub('[^a-zA-Zа-яА-Я1-9]+', ' ', sentence)
+    sentence = re.sub(' +',' ', sentence)
+    #text = re.sub(' [\w] ', ' ', text)
+    return [sentence.strip()]
+
 @app.route('/success/')
 def success():
-    if(session.get('user_text', None)):
-        return render_template('output.html')
-    else:
-        return render_template('emptyReview.html')
+    return render_template('output.html')
 
+@app.route('/noresponse/')
+def noresponse():
+    return render_template('emptyReview.html')
 
 @app.route('/home', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
         user_text = request.form['user_review']
+        if(not user_text):
+            return redirect(url_for('noresponse'))
         session['user_text'] = user_text
-        getScores()
+        session['processed_text'] = process_text1(user_text)
+        getScores(user_text)
         return redirect(url_for('success'))
-    else:
-        user = request.args.get('user_review')
-        return redirect(url_for('success'))
+    #else:
+    #    user = request.args.get('user_review')
+    #    return redirect(url_for('success'))
 
 @app.context_processor
 def score_processor():
@@ -35,13 +55,12 @@ def score_processor():
             return 'DeepPink'
         else:
             # some really funky math to get a good-looking gradient
-            
             # if score is within neutral range, map from rgb(100,100,100) at 0.0 to rgb(200,200,200) at +/-0.1
             if score >= -0.1 and score <= 0.1:
                 B = 100 + abs(score)*1000
             # otherwise, B=0 at +/-1, and linearly ramps to 135 at +/-0.1
             else:
-                B = (1-abs(score))*150
+                B = (1-abs(score))*175
             # if within neutral range, make the color grey
             if score <= 0.1:
                 G = B
